3eabb1aefcc0fa048104ebb293875222635df75a
Alex Wigforss <alex.wigforss@gmail.com>
2025-11-16 17:34:46 +0100

    Summary och återgå till meny

--- GodotSample/main.gd
+++ GodotSample/main.gd
@@ -1,16 +1,17 @@
 extends Node2D
 @onready var player = get_node("Player")
 @onready var label = get_node("DebugLabel")
-@onready var anim = get_node("AnimationPlayer")
 
-var player_speed = 400
-const w = 1920
-const h = 1080
+const width = 1920
+const height = 1080
+const half_width = int(round(width/2.0))
+const half_height = int(round(height/2.0))
+
 var pressed = false
-var pressed_position = Vector2(w/2,h/2)
 var game_over = false
 
 func _ready():
+	player.goto = Vector2(half_width,half_height)
 	update_label()
 
 func _on_ColorRect_gui_input(event):
@@ -19,25 +20,24 @@ func _on_ColorRect_gui_input(event):
 			pressed = true
 		elif event.pressed == false:
 			pressed = false
-	if event is InputEventMouseMotion:
-		pass
 
-func _process(delta):
+func _process(_delta):
 	if not game_over:
 		if pressed:
-			pressed_position = get_global_mouse_position()
-			
-		player.position  = player.position.move_toward(pressed_position, delta * player_speed)
-	# sprite.position.x  = move_toward(sprite.position.x, pressed_position.x, 10)
-	# sprite.position.y  = move_toward(sprite.position.y, pressed_position.y, 10)
+			player.goto = get_global_mouse_position()
+			# player.position  = player.position.move_toward(pressed_position, delta * player_speed)
+	elif game_over and pressed:
+		call_deferred('queue_free')
+	elif game_over:
+		stop_spawn_timers()
 
-func damage_taken():
-	anim.play("player_anim")
-	player.hp -= 1
-	update_label()
-	if player.hp <= 0:
-		label.text = str("Game Over")
-		game_over = true
 		
 func update_label():
-	label.text = "HP: " + str(player.hp) + " BULLETS: " + str(player.no_bullets)
+	label.text = "HÄLSA: " + str(player.hp) + " PROJEKTILER: " + str(player.no_bullets)
+
+func stop_spawn_timers():
+	$Picks/Timer.stop()
+	$Booos/Timer.stop()
+
+func summary():
+	label.text = "Game Over spelet är slut \n Klicka var som helst för att återgå till startmenyn!"
