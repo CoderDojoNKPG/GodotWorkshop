9a709ada491e78628488d4ca47fc750fdcc1b6d3
Alex Wigforss <alex.wigforss@gmail.com>
2025-11-16 17:31:07 +0100

    Bättre struktur och kommentarer
    Flyttat logik för position och hälsa från main till player

--- GodotSample/player.gd
+++ GodotSample/player.gd
@@ -1,10 +1,25 @@
 extends Area2D
 var bullet_scene: PackedScene = preload("res://actors/bullet.tscn")
 @onready var bullets = get_node("../Bullets")
+@onready var anim = get_node("../AnimationPlayer")
 
 var hp = 10
 var no_bullets = 0
+var goto = Vector2.ZERO
+@export var player_speed = 400
 
+func _process(delta: float) -> void:
+	position  = position.move_toward(goto, delta * player_speed)
+
+func damage_taken():
+	anim.play("player_anim")
+	hp -= 1
+	$"..".update_label()
+	if hp <= 0:
+		$"..".summary()
+		$"..".game_over = true
+
+# Hantering av projektiler
 func add_bullet():
 	no_bullets += 1
 	$"..".update_label()
@@ -19,5 +34,4 @@ func launch_bullet(ins):
 		instance.position = position
 		instance.pass_bullet_instance(ins)
 		bullets.add_child(instance)
-		print("PANG" +  str(ins.name))
 		sub_bullet()
