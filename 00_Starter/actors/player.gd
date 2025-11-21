a9aba7297297a872b16a57b5e0661c058e13d686
Alex Wigforss <alex.wigforss@gmail.com>
2025-11-21 17:36:37 +0100

    Flyttat rörelse till spelarobjektet

--- 00_Starter/actors/player.gd
+++ 00_Starter/actors/player.gd
@@ -1,17 +1,27 @@
 extends Area2D
 
+@onready var main = get_node("/root/Main")
 @onready var bullets = get_node("../Bullets")
 
 var hp = 10
 var no_bullets = 0
+var goto = Vector2.ZERO
+@export var speed = 400
+
+func _process(delta: float) -> void:
+	position  = position.move_toward(goto, delta * speed)
+
 
 func add_bullet():
 	no_bullets += 1
 	$"..".update_label()
 
+
 func sub_bullet():
 	no_bullets -= 1
 	$"..".update_label()
 
+
 func launch_bullet(ins):
+	# ATT GÖRA: Instansiera en projektil
 	pass
